103b4bd9f326296c2fce3f255c9594e9aa51f769
chadstachowicz <cstachowicz@Chads-iMac.local>
2015-07-26 14:08:31 -0400

    Added abiliy to do all in on bridge'

--- ChangeLightColorHue.java
+++ ChangeLightColorHue.java
@@ -5,15 +5,74 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import org.json.*;
+import java.util.Iterator;
 
 public class ChangeLightColorHue {
 
 	// http://localhost:8080/RESTfulExample/json/product/get
 	public static void main(String[] args) {
 
-	  try {
+    System.out.println(args[2]);
+    if (args[2].equals("All"))
+    {
 
-		URL url = new URL("http://" + args[0] + "/api/" + args[1] + "/lights/1/state");
+      try {
+      URL url = new URL("http://" + args[0] + "/api/" + args[1] + "/lights");
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.setRequestMethod("GET");
+      conn.setRequestProperty("Accept", "application/json");
+
+      if (conn.getResponseCode() != 200) {
+  			throw new RuntimeException("Failed : HTTP error code : "
+  					+ conn.getResponseCode());
+  		}
+
+  		BufferedReader br = new BufferedReader(new InputStreamReader(
+  			(conn.getInputStream())));
+
+  		String output;
+      String jsonData = "";
+  		System.out.println("Output from Server .... \n");
+  		while ((output = br.readLine()) != null) {
+            jsonData += output + "\n";
+  		}
+
+      JSONObject obj = new JSONObject(jsonData);
+      Iterator<?> keys = obj.keys();
+
+      while( keys.hasNext() ) {
+        String key = (String)keys.next();
+        System.out.println(key);
+        changeLights(args[0],args[1], key, args[3], args[4], args[5], args[6]);
+    }
+
+
+  		conn.disconnect();
+
+      } catch (MalformedURLException e) {
+
+  		e.printStackTrace();
+
+  	  } catch (IOException e) {
+
+  		e.printStackTrace();
+
+    } catch (JSONException e) {
+
+  		e.printStackTrace();
+
+    }
+      } else {
+        changeLights(args[0],args[1], args[2], args[3], args[4], args[5], args[6]);
+      }
+
+}
+
+  public static void changeLights(String ip, String key, String id, String on, String sat, String bri, String hue) {
+    try {
+
+		URL url = new URL("http://" + ip + "/api/" + key + "/lights/" + id + "/state");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("PUT");
     conn.setDoOutput(true);
@@ -21,7 +80,7 @@ public class ChangeLightColorHue {
 		conn.setRequestProperty("Accept", "application/json");
 
     OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
-        String json = "{\"on\": " + args[2] + ", \"sat\": " + args[3] + ", \"bri\": " + args[4] + ", \"hue\":" +  args[5] + "}";
+        String json = "{\"on\": " + on + ", \"sat\": " + sat + ", \"bri\": " + bri + ", \"hue\":" +  hue + "}";
         System.out.println(json);
         osw.write(json);
         osw.flush();
@@ -53,6 +112,6 @@ public class ChangeLightColorHue {
 
 	  }
 
-	}
+  }
 
 }
